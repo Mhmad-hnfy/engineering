9a8c2e6578ec31b3360b353180304390b50a9422
Mhmad-hnfy <hmwhnfy3@gmail.com>
2026-02-09 15:57:50 +0200

    pushing code

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  reactCompiler: true,
-  distDir: "dist",
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
